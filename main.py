e58a1b54497042ebf46a2e0ec99b10c42a40a651
S SIDHARTH <sidharthpunalur@gmail.com>
2026-02-23 01:37:01 +0530

    sensor code added

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+    price: float
+
 @app.get("/")
 def read_root():
-    return {"message": "AURA"}
+    return {"message": "Wifi Test Success"}
